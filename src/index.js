30c807836866ba440b3837947a6ddc68a90c01e3
leandrosteinberg <leandrosteinberg@gedyt.com.ar>
2025-11-03 18:22:37 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import GedytHealthForm from "./GedytHealthForm.jsx";
+import GedytHealthForm from "./GedytHealthForm";  // SIN .jsx
 
 const root = createRoot(document.getElementById("root"));
 root.render(<GedytHealthForm />);
